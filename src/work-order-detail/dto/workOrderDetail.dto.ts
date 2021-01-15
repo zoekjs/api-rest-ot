4d1a67a440fc5c2eaa1f9be04bcf90a5447762fb
zoek <felipe.esbu@gmail.com>
2021-01-15 20:38:33 -0300

    🎨

--- src/work-order-detail/dto/workOrderDetail.dto.ts
+++ src/work-order-detail/dto/workOrderDetail.dto.ts
@@ -2,10 +2,13 @@ import { IsNotEmpty } from "class-validator";
 
 export class CreateWorkOrderDetailDTO {
     @IsNotEmpty({ message: 'La cantidad es obligatoria'})
-    readonly quantity: number;
+    public readonly quantity: number;
+
     @IsNotEmpty({ message: 'La descripción es obligatoria'})
-    readonly description: string;
+    public readonly description: string;
+
     @IsNotEmpty({ message: 'El precio es obligatorio'})
-    readonly price: number;
-    readonly workOrderId: number;
+    public readonly price: number;
+
+    public readonly workOrderId: number;
 }

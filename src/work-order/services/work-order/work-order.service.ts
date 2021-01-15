d82f1fe8917d3ab9dc4a11d298e5ceea0957dab5
zoek <felipe.esbu@gmail.com>
2021-01-15 20:37:16 -0300

    🎨 Limpieza y formateo de código

--- src/work-order/services/work-order/work-order.service.ts
+++ src/work-order/services/work-order/work-order.service.ts
@@ -1,46 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateWorkOrderDTO } from '../../dto/workOrders.dto';
-import { WorkOrder as WorkOrderEnt} from '../../entities/workOrder.entity';
+import { WorkOrder as WorkOrderEnt } from '../../entities/workOrder.entity';
 import { WorkOrder } from '../../interfaces/workOrders.interface';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class WorkOrderService {
-    constructor(@InjectRepository(WorkOrderEnt) private readonly workOrderRepo: Repository<WorkOrder>){}
+    constructor(@InjectRepository(WorkOrderEnt) private readonly workOrderRepo: Repository<WorkOrder>) { }
 
-    async getWorkOrders(): Promise<WorkOrder[]> {
+    public async getWorkOrders(): Promise<WorkOrder[]> {
         const workOrders = await this.workOrderRepo.find();
-        console.log(workOrders)
-        return workOrders;   
+        return workOrders;
     }
 
-    async getWorkOrder(id: number): Promise<WorkOrder>{
+    public async getWorkOrder(id: number): Promise<WorkOrder> {
         const workOrder = await this.workOrderRepo.findOne(id);
         return workOrder;
     }
 
-    async createWorkOrder(createWorkOrderDTO: CreateWorkOrderDTO): Promise<WorkOrder> {
+    public async createWorkOrder(createWorkOrderDTO: CreateWorkOrderDTO): Promise<WorkOrder> {
         const workOrder = await this.workOrderRepo.create(createWorkOrderDTO);
-        
         return await this.workOrderRepo.save(workOrder);
     }
 
-    async updateWorkOrder(id: number, updateWorkOrderDTO: CreateWorkOrderDTO): Promise<WorkOrder> {
+    public async updateWorkOrder(id: number, updateWorkOrderDTO: CreateWorkOrderDTO): Promise<WorkOrder> {
         const workOrder = await this.workOrderRepo.findOne(id);
         await this.workOrderRepo.merge(workOrder, updateWorkOrderDTO);
         return await this.workOrderRepo.save(workOrder);
     }
-    async getWorkOrdersByClient(rut: number): Promise<WorkOrder[]>{
-        const workOrders = await this.workOrderRepo.find({ where: {client_rut: rut}, relations: ["client"]});
-        console.log(workOrders);
+    public async getWorkOrdersByClient(client: number): Promise<WorkOrder[]> {
+        const workOrders = await this.workOrderRepo.find({ where: { client: client } });
         return workOrders;
     }
-    async deleteWorkOrder(work_order_id: number) {
-        const { affected } = await this.workOrderRepo.delete(work_order_id);   
-        if(affected == 0){
+
+    public async deleteWorkOrder(work_order_id: number): Promise<Boolean> {
+        const { affected } = await this.workOrderRepo.delete(work_order_id);
+        if (affected == 0) {
             return false;
-        }else{
+        } else {
             return true;
         }
     }
